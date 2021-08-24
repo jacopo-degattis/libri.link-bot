e01b5cfdff6d11408f9fe2742439242523b4ede1
Paolo <jacopo.degattis@gmail.com>
2021-08-24 21:01:46 +0200

    added callback handler

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,6 +1,6 @@
 from config import TOKEN
-from commands.commands import start, search
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup,Update, Bot, ReplyKeyboardMarkup
+from commands.commands import start, search, inline_button
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Bot, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, CallbackContext
 
 def main():
@@ -9,10 +9,12 @@ def main():
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("search", search))
+    dispatcher.add_handler(CallbackQueryHandler(inline_button))
 
     updater.start_polling()
     updater.idle()
 
 if __name__ == "__main__":
+    global bot
     bot = Bot(TOKEN)
     main()
