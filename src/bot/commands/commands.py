fd8de81e2231f4a0c5b4e0ca7582aa3e1b175617
Jacopo <jacopo.degattis@gmail.com>
2021-08-28 12:06:26 +0200

    added download button for book, now bot sends download buttton along with book infos

--- src/bot/commands/commands.py
+++ src/bot/commands/commands.py
@@ -1,4 +1,3 @@
-from commands.api.core import *
 from commands.api.core import search as search_book, get_book_info, get_book_media_from_id
 from helper import *
 from telegram import InlineKeyboardMarkup
@@ -24,11 +23,21 @@ def search(update, context) -> None:
 def inline_button(update, context) -> None:
     query = update.callback_query.data
     
-    book_info = get_book_info(query)
-    book_cover = get_book_media_from_id(book_info["featured_media"])
+    if "download" in query:
+        # Dowload book
+        download_id = query.split("=")[1]
+        doc = get_buffer_book_download(download_id)
+        # TODO: fix filename part
+        update.callback_query.message.reply_document(doc, filename="test.epub")
+    else:
+        # get book info
+        # book_info = get_book_info(query)
+        # book_cover = get_book_media_from_id(book_info["featured_media"])
+        book_data = get_book_info(query)
+        book_infos = get_complete_book_data(book_data)
 
-    # TODO: creare una funzione per ottenere tutti i dati necessari
-    # da inviare nel messaggio -> serializzare i dati se necessario. es: titolo
+        # TODO: creare una funzione per ottenere tutti i dati necessari
+        # da inviare nel messaggio -> serializzare i dati se necessario. es: titolo
 
-    book_message = f"""\n📚 *Book Infos*\n\n*Title*: {serialize_title(html.unescape(book_info["title"]["rendered"]))}"""
-    update.callback_query.message.reply_photo(photo=book_cover, caption=book_message, parse_mode='Markdown')
+        book_message = f"""\n📚 *Book Infos*\n\n*Title*: {serialize_title(html.unescape(book_infos["title"]["rendered"]))}\n*Author*: {book_infos['author']}\n*Release Date*: {book_infos['year']}\n*Publisher*: {book_infos['publisher']}"""
+        update.callback_query.message.reply_photo(photo=book_infos["cover"], caption=book_message, parse_mode='Markdown', reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⬇️ Download", callback_data=f"download={book_infos['id']}")]]))
