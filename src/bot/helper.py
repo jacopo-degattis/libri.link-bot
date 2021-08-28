765aa74cfd7bfe4bfe8ce87300fbda127bfe0df7
Jacopo <jacopo.degattis@gmail.com>
2021-08-28 12:04:50 +0200

    added taxonomies fetch and function to get complete book data

--- src/bot/helper.py
+++ src/bot/helper.py
@@ -4,6 +4,7 @@ import requests
 from io import BytesIO
 from logging import currentframe
 from telegram import InlineKeyboardButton
+from commands.api.core import *
 
 def serialize_title(title):
   book_formats = ["epub", "mobi", "pdf"]
@@ -15,6 +16,38 @@ def get_raw_data_from_url(url):
   res = requests.get(url)
   raw_bytes_stream = BytesIO(res.content)
   return raw_bytes_stream
+
+def __fetch_taxonomies(taxonomies):
+  data = dict()
+  taxonomies_black_list = ["category", "post_tag"]
+  for tax in taxonomies:
+    if tax["taxonomy"] not in taxonomies_black_list:
+      res = requests.get(tax["href"]).json()
+      data[tax["taxonomy"]] = res
+  return data
+
+def get_complete_book_data(book_data):
+  # Function to get all necessary data for the book
+
+  # Fetch author info
+  taxonomies = book_data["_links"]["wp:term"]
+  taxonomies_info = __fetch_taxonomies(taxonomies)
+  
+  # TODO: add support for more than one author, genre, publisher etc.
+  # TODO: improve function, better algorithm
+  book_data["year"] = taxonomies_info["anno"][0]["name"] if len(taxonomies_info["anno"]) > 0 else "Unknown"
+  book_data["author"] = " ".join(taxonomies_info["autore"][0]["name"].split("-")).title() if len(taxonomies_info["autore"]) > 0 else "Unknown"
+  print(taxonomies_info["genere"])
+  book_data["genre"] = taxonomies_info["genere"][0]["name"] if len(taxonomies_info["genere"]) > 0 else None
+  book_data["publisher"] = " ".join(taxonomies_info["editore"][0]["name"].split("-")).title() if len(taxonomies_info["editore"]) > 0 else "Unknown"
+  book_data["cover"] = get_book_media_from_id(book_data["featured_media"])
+
+  return book_data
+
+def get_buffer_book_download(book_id):
+  uri = get_book_download_uri(book_id)[0]
+  document = get_raw_data_from_url(uri)
+  return document
 
 def get_inline_keboard_books(books, offset=5):
   # 5 books per page
